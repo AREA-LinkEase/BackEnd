db0e5d7ed7e6a01a57183f6d27417cc6553d200c
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-12-13 14:34:15 +0100

    [Feature](Controller/Models): new routes

--- app/controller/automates.js
+++ app/controller/automates.js
@@ -1,4 +1,5 @@
-import { createAutomate, deleteAutomate, getAllAutomates, getAutomateById } from "../model/automates.js"
+import { createAutomate, deleteAutomate, getAllAutomates, getAutomateById, updateAutomate } from "../model/automates.js"
+import { getWorkspaceById } from "../model/workspaces.js"
 
 export default function index(app) {
 
@@ -93,9 +94,13 @@ export default function index(app) {
             return response.status(500).json({error: error})
         }
     })
-    app.get('/automates/:automate_id', async (request, response) => {
+    app.get('/automates/:workspace_id/:automate_id', async (request, response) => {
         let automate_id = request.params.automate_id
+        let workspace_id = request.params.workspace_id
 
+        let workspace = await getWorkspaceById(workspace_id)
+        if (workspace === null)
+            return response.status(404).json({error: "Unkwnown workspace"})
         try {
             let json = await getAutomateById(automate_id)
             return response.status(200).json({result: json})
@@ -104,16 +109,21 @@ export default function index(app) {
             return response.status(500).json({error: error})
         }
     })
-    app.post('/automates', async (request, response) => {
+    app.post('/automates/:workspace_id', async (request, response) => {
+        let workspace_id = request.params.workspace_id
         let body = request.body
 
         if (body.title === undefined || body.workspace_id === undefined || body.workflow === undefined,
             body.variables === undefined || body.secrets === undefined)
             return response.status(422).json({error: "missing field"})
+        let workspace = await getWorkspaceById(workspace_id)
+        console.log(workspace);
+        if (workspace === null)
+            return response.status(404).json({error: "unknown workspace"})
         try {
             let json = await createAutomate(
                 body.title,
-                body.workspace_id,
+                workspace_id,
                 body.workflow,
                 body.variables,
                 body.secrets
@@ -124,9 +134,32 @@ export default function index(app) {
             return response.status(500).json({error: error})
         }
     })
-    app.delete('/automates/:automate_id', async (request, response) => {
+    app.put('/automates/:workspace_id/:automate_id', async (request, response) => {
+        let workspace_id = request.params.workspace_id
         let automate_id = request.params.automate_id
+        let body = request.body
+    
+        let workspace = await getWorkspaceById(workspace_id)
+        if (workspace === null)
+            return response.status(404).json({error: "unknown workspace"})
+        try {
+            let json = await updateAutomate(automate_id, body)
+            return response.status(200).json({result: "Automate updated successfully"})
+        } catch(error) {
+            console.log(error);
+            //TODO faire une erreur custom
+            // if (error === "Workspace ID cannot be updated.")
+            //     return response.status(222).json({error: "workspace_id can't be updated"})
+            return response.status(500).json({error: error})
+        }
+    })
+    app.delete('/automates/:workspace_id/:automate_id', async (request, response) => {
+        let automate_id = request.params.automate_id
+        let workspace_id = request.params.workspace_id
 
+        let workspace = getWorkspaceById(workspace_id)
+        if (workspace === null)
+            return response.statsu(404).json({error: "Unknown workspace"})
         try {
             let json = await deleteAutomate(automate_id)
             return response.status(200).json({result: "Automate deleted successfully"})
