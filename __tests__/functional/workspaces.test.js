6b34b911a530d5fab2eac04e10fee821e7455e48
Kéziah IMER <keziah.imer@epitech.eu>
2023-12-16 16:21:04 +0100

    [Tests](Workspace): add reponse content test

--- __tests__/functional/workspaces.test.js
+++ __tests__/functional/workspaces.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../config/express.js';
 import {setupTest} from "../../testBase.js";
 import {getSequelize} from "../../app/getDataBaseConnection.js";
+import { expect } from '@jest/globals';
 
 let id
 
@@ -14,55 +15,87 @@ afterAll(() => {
 })
 
 describe('POST /workspaces', () => {
-    test('should respond with status 200 and a JSON object', async () => {
+    test('should create a new workspace', async () => {
         const workspaceData = {
             title: "workspace_test",
             description: "workspace created with jest",
             is_private: false
         }
-        const response = await request(app).post('/workspaces').send(workspaceData).set('Accept', 'application/json');
+        const response = await request(app)
+            .post('/workspaces')
+            .send(workspaceData)
+            .set('Accept', 'application/json');
 
         expect(response.status).toBe(201);
     });
 });
 
 describe('GET /workspaces', () => {
-    test('should respond with status 200 and a JSON object', async () => {
+    test('should get all workspaces', async () => {
         const response = await request(app).get('/workspaces');
         
-        expect(response.status).toBe(200);
         id = response.body.result[response.body.result.length - 1].id
+        expect(response.status).toBe(200);
+        response.body.result.forEach(workspace => {
+            expect(workspace).toHaveProperty('title');
+            expect(workspace).toHaveProperty('description');
+            expect(workspace).toHaveProperty('is_private');
+        });
     });
 });
 
 describe('GET /workspaces/{workspace_id}', () => {
-    test('should respond with status 200 and a JSON object', async () => {
+    test('should get a workspace by ID', async () => {
         const response = await request(app).get(`/workspaces/${id}`);
 
         expect(response.status).toBe(200);
-        console.log(response.body)
+        expect(response.body.result).toHaveProperty('title');
+        expect(response.body.result).toHaveProperty('description');
+        expect(response.body.result).toHaveProperty('is_private');
     });
 });
 
+describe('PUT /workspaces/{workspace_id}', () => {
+    test('should modify a workspace by ID', async () => {
+        const updatedWorkspace = { title: 'Workspace Modifié' };
+        const response = await request(app)
+            .put(`/workspaces/${id}`)
+            .send(updatedWorkspace);
+    
+        expect(response.status).toBe(200);
+      });
+})
+
 describe('GET /workspaces/private', () => {
-    test('should respond with status 200 and a JSON object', async () => {
+    test('should get all private workspaces', async () => {
         const response = await request(app).get('/workspaces/private');
 
         expect(response.status).toBe(200);
+        response.body.result.forEach(workspace => {
+            expect(workspace).toHaveProperty('title');
+            expect(workspace).toHaveProperty('description');
+            expect(workspace).toHaveProperty('is_private');
+            expect(workspace.is_private).toBe(true)
+        });
     });
 });
 
 describe('GET /workspaces/public', () => {
-    test('should respond with status 200 and a JSON object', async () => {
+    test('should get all public workspaces', async () => {
         const response = await request(app).get('/workspaces/public');
         
         expect(response.status).toBe(200);
+        response.body.result.forEach(workspace => {
+            expect(workspace).toHaveProperty('title');
+            expect(workspace).toHaveProperty('description');
+            expect(workspace).toHaveProperty('is_private');
+            expect(workspace.is_private).toBe(false)
+        });
     });
 });
 
 describe('DELETE /workspaces/{workspace_id}', () => {
-    test('should respond with status 200 and a JSON object', async () => {
-        console.log(id)
+    test('should delete a workspace by ID', async () => {
         const response = await request(app).delete(`/workspaces/${id}`);
 
         expect(response.status).toBe(200);
