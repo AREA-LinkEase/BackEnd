bd39425468e9b8d8998ae2dbd6c5f83b75b4f054
Younes Bahri <91877321+bahmez@users.noreply.github.com>
2023-12-16 15:51:30 +0100

    Revert "Lin 40 61 67 workspaces"

--- app/testSync.js
+++
@@ -1,7 +0,0 @@
-import { User } from './model/users.js';
-import { Automate } from './model/automates.js'
-import { Workspace } from './model/workspaces.js'
-
-User.sync()
-Automate.sync()
-Workspace.sync()
