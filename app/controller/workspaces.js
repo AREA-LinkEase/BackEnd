c88cf0d597a88e322a6b537db348fbc03e70c95f
SimonVermeulen <91828331+SimonVermeulen@users.noreply.github.com>
2023-12-19 21:49:25 +0100

    Update workspaces.js

--- app/controller/workspaces.js
+++ app/controller/workspaces.js
@@ -407,7 +407,7 @@ export default function index(app) {
             let json = await getAllWorkspaces(payload.id)
             return response.status(200).json({result: json})
         } catch(error) {
-            return response.status(500).json({result: error})
+            return response.status(500).json({result: error.toString()})
         }
     })
     app.put('/workspaces/:workspace_id', async (request, response) => {
