5b19bd195f913c3c52d1349b68f2489739bbc60b
IKEA.luddes <clement.pecas33@gmail.com>
2023-12-23 18:25:44 +0100

    [Feat] (models) : sync model to database schema

--- app/model/services.js
+++ app/model/services.js
@@ -1,35 +1,57 @@
 import { DataTypes } from 'sequelize'
 import { getSequelize } from '../getDataBaseConnection.js'
+import {User} from "./users.js";
 
-const Services = getSequelize().define('Service', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
+const Services = getSequelize().define('services', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_secret: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    scope: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    auth_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    token_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    owner_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
         },
-        client_id: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        client_secret: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        scope: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        auth_url: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        token_url: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    }, {
-        timestamps: false,
-    }
-)
+    },
+    users_id: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: '[]',
+    },
+    is_private: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+});
 
 export async function getServicesById(id) {
     const service = await Services.findOne({
