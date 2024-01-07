3bfd6cbc85beafbf557f2e267d0298949d1a6833
SimonVermeulen <91828331+SimonVermeulen@users.noreply.github.com>
2024-01-07 05:43:04 +0100

    [Test](Auth): tests of auth routes

--- __tests__/functional/auth.test.js
+++ __tests__/functional/auth.test.js
@@ -89,4 +89,35 @@ describe('POST /auth/register', () => {
 
         expect(response.status).toBe(409);
     });
+    test('should redirect to Discord OAuth login', async () => {
+        client_id = process.env.DISCORD_CLIENT_ID;
+        redirect_uri = process.env.DISCORD_REDIRECT_URI;
+
+        const response = await request(app).get('/auth/login/discord');
+        expect(response.status).toBe(302);
+    
+        const expectedRedirectUri = `https://discord.com/api/oauth2/authorize?client_id=` + client_id + `&redirect_uri=` + +redirect_uri  `&response_type=code&scope=identify%20email`;
+        expect(response.header['location']).toBe(expectedRedirectUri);
+    });
+    test('should redirect to GitHub OAuth login', async () => {
+        client_id = process.env.DISCORD_CLIENT_ID;
+        redirect_uri = process.env.DISCORD_REDIRECT_URI;
+    
+        const response = await request(app).get('/auth/login/github');
+        expect(response.status).toBe(302);
+    
+        const expectedRedirectUri = `https://github.com/login/oauth/authorize?client_id=` + client_id + `&redirect_uri=` + redirect_uri + `&response_type=code`;
+        expect(response.header['location']).toBe(expectedRedirectUri);
+    });
+    
+    test('should redirect to Google OAuth login', async () => {
+        client_id = process.env.DISCORD_CLIENT_ID;
+        redirect_uri = process.env.DISCORD_REDIRECT_URI;
+    
+        const response = await request(app).get('/auth/login/google');
+        expect(response.status).toBe(302);
+    
+        const expectedRedirectUri = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=` + client_id + `&redirect_uri=` + redirect_uri + `&scope=https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email`;
+        expect(response.header['location']).toBe(expectedRedirectUri);
+    });
 });
