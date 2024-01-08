b6342aa02af2f7fcc10006745b16e339f90bf692
IKEA.luddes <clement.pecas33@gmail.com>
2024-01-08 07:51:57 +0100

    [Feat] (auth/services) : add final_redirect and update download_avatar

--- app/utils/download_avatar.js
+++ app/utils/download_avatar.js
@@ -1,13 +1,14 @@
-import fetch from 'node-fetch'
-import fs from 'fs'
+import * as fs from 'fs';
+import * as path from "path";
+import {finished} from "stream/promises";
+import {Readable} from "stream";
 
-async function downloadAvatar(url, username) {
+async function downloadAvatar(url, id) {
     try {
-        const response = await fetch(url);
-        if (!response.ok)
-            throw new Error('Unable to fetch avatar.');
-        const imageBuffer = await response.buffer()
-        fs.writeFileSync(username + '.png', imageBuffer);
+        const imageResponse = await fetch(url);
+        const destination = path.resolve(process.cwd() + `/public/avatars`, `${id}.png`)
+        const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+        await finished(Readable.fromWeb(imageResponse.body).pipe(fileStream));
     } catch (error) {
         console.error('Error downloading the avatar:', error);
     }
